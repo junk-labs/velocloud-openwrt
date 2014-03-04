f63f483040cb756bafd47ebeceaf1d5ef8488aa4
Shankar Unni <shankar@velocloud.net>
2014-03-04 13:24:49 -0800

    Revert "Merge branch 'master' of git.assembla.com:velocloud.openwrt"
    This reverts commit 0e7b203b2119900be100f42db801c0e1113b9c21, reversing
    changes made to 4dda87a6d1fb5aa99fa5f423fb02635582896a92.

--- trunk/target/linux/x64/image/gen_image_velocloud.sh
+++ trunk/target/linux/x64/image/gen_image_velocloud.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 # Copyright (C) 2013 Veloccloud.net
 set -x
-[ $# -ge 7 ] || {
-    echo "SYNTAX: $0 <file> <boot size> <boot dir> <rootfs size> <rootfs image> <install root size> <install dirs> ..."
+[ $# -ge 8 ] || {
+    echo "SYNTAX: $0 <file> <align> <boot size> <boot dir> <rootfs size> <rootfs image> <install root size> <install dirs> ..."
     exit 1
 }
 
 ARGV=("$@")
 OUTPUT="$1"
-BOOT_SIZE="$2"
-BOOT_DIR="$3"
-ROOT_SIZE="$4"
-ROOT_FSIMAGE="$5"
-INST_ROOT_SIZE="$6"
-INST_DIR="$7"
+ALIGN="$2"
+BOOT_SIZE="$3"
+BOOT_DIR="$4"
+ROOT_SIZE="$5"
+ROOT_FSIMAGE="$6"
+INST_ROOT_SIZE="$7"
+INST_DIR="$8"
 
 # translate install images dirs;
 # first inst_dir entry must point to a dir without ':' separator;
@@ -23,7 +24,7 @@ INST_PATH="images"
 
 root_dirs=""
 inst_dirs=""
-i=6
+i=7
 while [ $i -lt $# ]
 do
 	arg=${ARGV[$i]}
@@ -34,82 +35,52 @@ do
 done
 
 rm -f "$OUTPUT"
-rm -rf $IROOT
 
-# align all partition to 1MB boundaries;
+# fix to align to 1MB boundaries;
+
+head=32
+sect=64
+cyl=$(( ($BOOT_SIZE + $ROOT_SIZE) * 1024 * 1024 / ($head * $sect * 512)))
+
+# create partition table
+# the installer uses plain old MSDOS format;
 # root and installer filesystem are same size;
 # INST_ROOT_SIZE is a parameter to the installer;
 
-GRUB_OFF=1
-GRUB_END=2
+set `ptgen -o "$OUTPUT" -h $head -s $sect -p ${BOOT_SIZE}m -p ${ROOT_SIZE}m -p ${ROOT_SIZE}m ${ALIGN:+-l $ALIGN}`
 
-BOOT_OFF=$GRUB_END
-BOOT_END="$(($BOOT_OFF + $BOOT_SIZE))"
-
-ROOT_OFF=$BOOT_END
-ROOT_END="$(($ROOT_OFF + $ROOT_SIZE))"
-
-INST_OFF=$ROOT_END
-INST_END="$(($INST_OFF + $ROOT_SIZE))"
-
-TAIL_OFF=$INST_END
-TAIL_SIZE=1
-TAIL_END="$(($TAIL_OFF + $TAIL_SIZE))"
+BOOT_OFFSET="$(($1 / 512))"
+BOOT_SIZE="$(($2 / 512))"
+ROOT_OFFSET="$(($3 / 512))"
+ROOT_SIZE="$(($4 / 512))"
+INST_OFFSET="$(($5 / 512))"
+INST_SIZE="$(($6 / 512))"
 
 # copy rootfs image;
 
-dd if="$ROOT_FSIMAGE" of="$OUTPUT" bs=1M seek="$ROOT_OFF" conv=notrunc count="$ROOT_SIZE"
-dd if=/dev/zero of="$OUTPUT" bs=1M seek="$TAIL_OFF" conv=notrunc count="$TAIL_SIZE"
+dd if="$ROOT_FSIMAGE" of="$OUTPUT" bs=512 seek="$ROOT_OFFSET" conv=notrunc count="$ROOT_SIZE"
 
 # copy installer trees;
 
-BLOCKS="$(($ROOT_SIZE * 1024))"
+BLOCKS="$(($INST_SIZE / 2))"
 
 mkdir -p $root_dirs
 echo $INST_ROOT_SIZE > $IROOT/$INST_PATH/root-size
 echo $GRUB2_MODULES > $IROOT/$INST_PATH/grub-modules
-$STAGING_DIR_HOST/bin/genext2fs -U -i 8192 -d "$IROOT" -b "$BLOCKS" "$OUTPUT.inst"
+genext2fs -U -i 8192 -d "$IROOT" -b "$BLOCKS" "$OUTPUT.inst"
 rm -rf "$IROOT"
-$STAGING_DIR_HOST/bin/genext2fs -U -x "$OUTPUT.inst" $inst_dirs -b "$BLOCKS" "$OUTPUT.inst"
-INST_UUID=`uuidgen`
-$STAGING_DIR_HOST/bin/tune2fs -O extents,uninit_bg,dir_index -U $INST_UUID "$OUTPUT.inst"
-$STAGING_DIR_HOST/bin/e2fsck -fy "$OUTPUT.inst"
 
-dd if="$OUTPUT.inst" of="$OUTPUT" bs=1M seek="$INST_OFF" conv=notrunc
+genext2fs -U -x "$OUTPUT.inst" $inst_dirs -b "$BLOCKS" "$OUTPUT.inst"
+dd if="$OUTPUT.inst" of="$OUTPUT" bs=512 seek="$INST_OFFSET" conv=notrunc
 rm -rf "$OUTPUT.inst"
 
-# create gpt partition table;
-
-parted -s -- $OUTPUT \
-	unit MiB \
-	mklabel gpt \
-	mkpart grub $GRUB_OFF $GRUB_END \
-	set 1 bios_grub on \
-	mkpart boot $BOOT_OFF $BOOT_END \
-	set 2 boot on \
-	mkpart root $ROOT_OFF $ROOT_END \
-	set 3 boot off \
-	mkpart inst $INST_OFF $INST_END \
-	set 4 boot off \
-	print \
-	unit s \
-	print
-
-# extract root partition uuid;
-# overwrite root=PARTUUID with root uuid;
-
-ROOT_UUID=`sgdisk -i 3 $OUTPUT | grep 'unique GUID' | sed -e 's/^.*: //'`
-sed -i \
-	-e "s/@PARTUUID@/PARTUUID=$ROOT_UUID/g" \
-	-e "s/vc.install/vc.install=$INST_UUID/" \
-	$BOOT_DIR/boot/grub/grub.cfg
-
-# make final boot partition;
 # install grub;
 
-BLOCKS="$(($BOOT_SIZE * 1024))"
+[ -n "$NOGRUB" ] && exit 0
 
-$STAGING_DIR_HOST/bin/genext2fs -U -i 8192 -d "$BOOT_DIR" -b "$BLOCKS" "$OUTPUT.boot"
-dd if="$OUTPUT.boot" of="$OUTPUT" bs=1M seek="$BOOT_OFF" conv=notrunc
+BLOCKS="$(($BOOT_SIZE / 2))"
+
+genext2fs -U -i 8192 -d "$BOOT_DIR" -b "$BLOCKS" "$OUTPUT.boot"
+dd if="$OUTPUT.boot" of="$OUTPUT" bs=512 seek="$BOOT_OFFSET" conv=notrunc
 rm -f "$OUTPUT.boot"
 

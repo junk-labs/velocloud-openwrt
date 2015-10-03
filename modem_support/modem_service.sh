1f3b46b057a73b4443fa0f85e3d49d2d3b792423
Aleksander Morgado <aleksander@aleksander.es>
2015-10-03 16:39:31 +0200

    minor: cleanup tab vs spaces indentation
    Try to consolidate the indentation used in each file.

--- modem_support/modem_service.sh
+++ modem_support/modem_service.sh
@@ -5,17 +5,17 @@ USB=$1
 trigger=$2
 
 log() {
-        logger -t "usbmodems" "$@"
+	logger -t "usbmodems" "$@"
 }
 
 get_bridge_interface() {
-        echo $(uci -X show network | \
-                  awk -F '=|\.' '/network.network[0-9]*=interface/ { print "br-"$2}')
+	echo $(uci -X show network | \
+		      awk -F '=|\.' '/network.network[0-9]*=interface/ { print "br-"$2}')
 }
 
 get_usb_ifname() {
 	local usb=$1
-        echo $(uci -c $modem_path get modems.$usb.ifname)
+	echo $(uci -c $modem_path get modems.$usb.ifname)
 }
 
 get_usb_type() {
@@ -24,8 +24,8 @@ get_usb_type() {
 }
 
 get_all_available_usb() {
-        echo $(uci -c $modem_path show modems |
-            awk -F'=|\.' '/modems.*=interface/ {print $2}')
+	echo $(uci -c $modem_path show modems | \
+		      awk -F'=|\.' '/modems.*=interface/ {print $2}')
 }
 
 get_edge_activatedstate() {
@@ -34,7 +34,7 @@ get_edge_activatedstate() {
 	# edged running
 		if [ -f "/opt/vc/.edge.info" ]; then
 			echo 1
-			return 
+			return
 		fi
 	fi
 	echo 0
@@ -58,19 +58,19 @@ modems_fwd_bridge_usblist() {
 	local usblist=$1
 	local insert_delete_rule=$2
 	local accept_drop_action=$3
-	local usb usbifname 
+	local usb usbifname
 	local brintf brinterfaces
-        if [ ! -n "$usblist" ];
-        then
-                return
-        fi
-        brinterfaces=$(get_bridge_interface)
+	if [ ! -n "$usblist" ];
+	then
+		return
+	fi
+	brinterfaces=$(get_bridge_interface)
 	for usb in $usblist
 	do
 		usbifname=$(get_usb_ifname $usb)
 		for brintf in $brinterfaces
 		do
-		    modems_forward_rule $brintf $usbifname $insert_delete_rule $accept_drop_action
+			modems_forward_rule $brintf $usbifname $insert_delete_rule $accept_drop_action
 		done
 	done
 }
@@ -79,10 +79,10 @@ modems_nat_usblist() {
 	local usblist=$1
 	local action=$2
 	local usb usbifname
-        if [ ! -n "$usblist" ];
-        then
-                return
-        fi
+	if [ ! -n "$usblist" ];
+	then
+		return
+	fi
 	for usb in $usblist
 	do
 		usbifname=$(get_usb_ifname $usb)
@@ -100,131 +100,130 @@ modems_plugin_add_rule() {
 		modems_fwd_bridge_usblist "$usb" "D" "DROP"
 		modems_fwd_bridge_usblist "$usb" "I" "ACCEPT"
 	fi
-	modems_nat_usblist "$usb" "I" 
+	modems_nat_usblist "$usb" "I"
 }
 
 modems_plugout_delete_rule() {
 	local usb=$1
 	modems_fwd_bridge_usblist "$usb" "D" "ACCEPT"
 	modems_fwd_bridge_usblist "$usb" "D" "DROP"
-	modems_nat_usblist "$usb" "D" 
+	modems_nat_usblist "$usb" "D"
 }
 
 modems_edged_run_add_rule() {
-        local usball=$(get_all_available_usb)
+	local usball=$(get_all_available_usb)
 	modems_fwd_bridge_usblist "$usball" "D" "ACCEPT"
 	modems_fwd_bridge_usblist "$usball" "D" "DROP"
 	modems_fwd_bridge_usblist "$usball" "I" "DROP"
-	
-    #configure IPtables to permit making REST API calls to IP modems
-    iptables -D VCMP_IN_ACL --source 192.168.32.2 -j ACCEPT
-    iptables -I VCMP_IN_ACL --source 192.168.32.2 -j ACCEPT
-    iptables -D VCMP_IN_ACL --source 192.168.14.1 -j ACCEPT
-    iptables -I VCMP_IN_ACL --source 192.168.14.1 -j ACCEPT
-    # edged also sucks in the modem http response packets and spits it
-    # out on vce1, the pantech modems specifically are interface-bound
-    # via curl --interface option, so a tcp packet with the same tuple
-    # but on a different interface (vce1) will send a RST to the peer
-    iptables -I INPUT -i vce1 -s 192.168.32.2 -p tcp --sport 80 -j DROP
+
+	#configure IPtables to permit making REST API calls to IP modems
+	iptables -D VCMP_IN_ACL --source 192.168.32.2 -j ACCEPT
+	iptables -I VCMP_IN_ACL --source 192.168.32.2 -j ACCEPT
+	iptables -D VCMP_IN_ACL --source 192.168.14.1 -j ACCEPT
+	iptables -I VCMP_IN_ACL --source 192.168.14.1 -j ACCEPT
+	# edged also sucks in the modem http response packets and spits it
+	# out on vce1, the pantech modems specifically are interface-bound
+	# via curl --interface option, so a tcp packet with the same tuple
+	# but on a different interface (vce1) will send a RST to the peer
+	iptables -I INPUT -i vce1 -s 192.168.32.2 -p tcp --sport 80 -j DROP
 }
 
 modems_edged_kill_delete_rule() {
-        local usball=$(get_all_available_usb)
+	local usball=$(get_all_available_usb)
 	modems_fwd_bridge_usblist "$usball" "D" "ACCEPT"
 	modems_fwd_bridge_usblist "$usball" "D" "DROP"
 	modems_fwd_bridge_usblist "$usball" "I" "ACCEPT"
-    #Remove IPtable rules that was permiting REST API calls to IP modems
-    iptables -D VCMP_IN_ACL --source 192.168.32.2 -j ACCEPT
-    iptables -D VCMP_IN_ACL --source 192.168.14.1 -j ACCEPT
-    iptables -D INPUT -i vce1 -s 192.168.32.2 -p tcp --sport 80 -j DROP
+	#Remove IPtable rules that was permiting REST API calls to IP modems
+	iptables -D VCMP_IN_ACL --source 192.168.32.2 -j ACCEPT
+	iptables -D VCMP_IN_ACL --source 192.168.14.1 -j ACCEPT
+	iptables -D INPUT -i vce1 -s 192.168.32.2 -p tcp --sport 80 -j DROP
 }
 
 modems_insert_fw_allusb() {
-        local usball=$(get_all_available_usb)
+	local usball=$(get_all_available_usb)
 	modems_plugin_add_rule "$usball"
 }
 
 modems_delete_fw_allusb() {
-        local usball=$(get_all_available_usb)
+	local usball=$(get_all_available_usb)
 	modems_plugout_delete_rule "$usball"
 }
 
 modems_start() {
-        local type
-        modems_stop
+	local type
+	modems_stop
 	type=$(get_usb_type $USB)
-        log "$USB: Start the modem $type script"
-        if [ "$type" == "serial" ];then
-                nohup $modem_script_path/TTYModems/ppp_run.sh -ifname $USB 2>&1 >/dev/null &
-        elif [ "$type" == "qmihybrid" ];then
-                nohup $modem_script_path/TTYModems/qmi_connect.sh -ifname $USB 2>&1 >/dev/null &
-        else
-                nohup $modem_script_path/IPModems/IPModemsRun.py $USB 2>&1 >/dev/null &
-        fi
+	log "$USB: Start the modem $type script"
+	if [ "$type" == "serial" ];then
+		nohup $modem_script_path/TTYModems/ppp_run.sh -ifname $USB 2>&1 >/dev/null &
+	elif [ "$type" == "qmihybrid" ];then
+		nohup $modem_script_path/TTYModems/qmi_connect.sh -ifname $USB 2>&1 >/dev/null &
+	else
+		nohup $modem_script_path/IPModems/IPModemsRun.py $USB 2>&1 >/dev/null &
+	fi
 	modems_plugin_add_rule "$USB"
-        log "$USB: Started modem $type script sucessfully"
+	log "$USB: Started modem $type script sucessfully"
 }
 
 modems_stop() {
-        local pid=$(cat /$tmpdir/$USB.pid)
-        local type=$(get_usb_type $USB)
-        kill -SIGTERM $pid
-        if [ "$type" != "serial" ];then
+	local pid=$(cat /$tmpdir/$USB.pid)
+	local type=$(get_usb_type $USB)
+	kill -SIGTERM $pid
+	if [ "$type" != "serial" ];then
 		kill -SIGKILL $pid
 	fi
 	modems_plugout_delete_rule "$USB"
-        log "$USB: Kill modem $type script"
+	log "$USB: Kill modem $type script"
 }
 
 modems_restart() {
-        local type=$(get_usb_type $USB)
-        log "$USB: Restarting the modem $type script"
-        modems_stop
-        sleep 5
-        modems_start
-        log "$USB: Restart done."
+	local type=$(get_usb_type $USB)
+	log "$USB: Restarting the modem $type script"
+	modems_stop
+	sleep 5
+	modems_start
+	log "$USB: Restart done."
 }
 
 modems_status() {
-        local type=$(get_usb_type $USB)
-        log "$USB: Check for the modem $type status"
-        $(uci -c $modem_path get modems.$USB.ifname >/dev/null 2>&1)
-        if [ $? != 0 ]; then
-                # Modem not detected or not inserted
-                echo "None"
-                return
-        fi
-        if [ -f /$tmpdir/$USB.pid ];then
-                echo "Running"
-        else
-                echo "Stopped"
-        fi
+	local type=$(get_usb_type $USB)
+	log "$USB: Check for the modem $type status"
+	$(uci -c $modem_path get modems.$USB.ifname >/dev/null 2>&1)
+	if [ $? != 0 ]; then
+		# Modem not detected or not inserted
+		echo "None"
+		return
+	fi
+	if [ -f /$tmpdir/$USB.pid ];then
+		echo "Running"
+	else
+		echo "Stopped"
+	fi
 }
 
 case "$trigger" in
-        'start')
-                modems_start
-          ;;
-        'stop')
-                modems_stop
-          ;;
-        'restart')
-                modems_restart
-          ;;
-        'status')
-                modems_status
-          ;;
-        'edged')
-                modems_edged_run_add_rule
-          ;;
-        'noedged')
-                modems_edged_kill_delete_rule
-          ;;
+	'start')
+		modems_start
+	  ;;
+	'stop')
+		modems_stop
+	  ;;
+	'restart')
+		modems_restart
+	  ;;
+	'status')
+		modems_status
+	  ;;
+	'edged')
+		modems_edged_run_add_rule
+	  ;;
+	'noedged')
+		modems_edged_kill_delete_rule
+	  ;;
 	'fwrestart')
 		modems_insert_fw_allusb
 	  ;;
-        *)
-                echo "Usage: $0 USB<number> start|stop|restart"
-                echo "       $0 usball edged|noedged|fwrestart"
+	*)
+		echo "Usage: $0 USB<number> start|stop|restart"
+		echo "	     $0 usball edged|noedged|fwrestart"
 esac
-

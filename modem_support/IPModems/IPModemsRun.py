ec63fbe2ed9fdd9ae82ffbc3d2dea91bd68445a7
Aleksander Morgado <aleksander@aleksander.es>
2015-10-09 16:30:37 +0200

    re#12: IPModems: use a QMI-based implementation for the UML290
    The logic which decides which plugin to use with each device is now updated to
    allow specifying a USB VID:PID pair. We use this new logic to map the UML290
    with the new 'uml290' plugin.

--- modem_support/IPModems/IPModemsRun.py
+++ modem_support/IPModems/IPModemsRun.py
@@ -11,10 +11,15 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 def supported_modems():
-    logging.debug("Supported Modems")
+    logging.debug("Supported vid/pids:")
     logging.debug("================");
-    for modem in supported_modem_list:
-        logging.debug(modem)
+    for vidpid, plugin in supported_vidpid_list.iteritems():
+        logging.debug("  " + vidpid + ": " + plugin + " plugin")
+    logging.debug("================");
+    logging.debug("Supported vendors:")
+    logging.debug("================");
+    for vendor_string, plugin in supported_vendor_string_list.iteritems():
+        logging.debug("  " + vendor_string + ": " + plugin + " plugin")
     logging.debug("================");
 
 #
@@ -29,40 +34,63 @@ debugflag = logging.WARNING #logging.DEBUG
 debugfile = '/tmp/USB/'+USB+'_ipmodem_info_collector.log'
 logging.basicConfig(filename=debugfile, level=debugflag)
 
-supported_modem_list = {
-        'Beceem Communications' : 'ubee',
-        'Pantech' : 'pantech',
-        'ZTE,Incorporated' : 'zte',
-        'Sierra Wireless, Incorporated' : 'sierra'
-        }
+# This is a VID/PID based list
+supported_vidpid_list = {
+    '106c:3718' : 'uml290'
+}
+
+# This is a Vendor string based list
+supported_vendor_string_list = {
+    'Beceem Communications'         : 'ubee',
+    'Pantech'                       : 'pantech',
+    'ZTE,Incorporated'              : 'zte',
+    'Sierra Wireless, Incorporated' : 'sierra'
+}
 
 supported_modems() #print the supported modems
 modemobj = IPModems.IPModems(USB)
 modemobj.set_link_id()
 
-found = 0
 while True:
     try:
-        manufacturer = modemobj.manufacturer
-        for modem in supported_modem_list:
-            if manufacturer.find(modem) >= 0:
-                found = 1
-                modem_str = supported_modem_list[modem]
-                modem_ustr = modem_str.capitalize()
-                exec "from %s import %s" %(modem_str, modem_ustr)
-                exec "modemobj = %s('%s')" %(modem_ustr, USB)
-                try:
-                    logging.warning("[manufacturer=%s], starting collecting info", modem);
-                    modemobj.get_modem_info()
-                    logging.warning("[manufacturer=%s], info collection completed", modem);
-                except NameError:
-                    logging.error("NameError..");
-        if not found:
-            logging.warning("manufacturer %s not in supported modem list", manufacturer);
+        found_plugin = ''
+        found_plugin_id = ''
+
+        # Look for plugin to use based on VID/PID
+        device_vidpid = modemobj.product
+        for vidpid, plugin in supported_vidpid_list.iteritems():
+            if device_vidpid.find(vidpid) >= 0:
+                found_plugin = plugin
+                found_plugin_id = vidpid
+                break
+
+        # Look for plugin to use based on vendor string
+        if not found_plugin:
+            device_vendor_string = modemobj.manufacturer
+            for vendor_string, plugin in supported_vendor_string_list.iteritems():
+                if device_vendor_string.find(vendor_string) >= 0:
+                    found_plugin = plugin
+                    found_plugin_id = vendor_string
+                    break
+
+        # Error out if no plugin really found
+        if not found_plugin:
+            logging.warning("manufacturer %s not in supported modem list", device_vendor_string);
             modemobj.set_link_id()
             '''All mandatory values can be set here and implemented in the base
             class'''
             sys.exit(0)
+
+        obj_name = found_plugin.capitalize()
+        exec "from %s import %s" %(found_plugin, obj_name)
+        exec "modemobj = %s('%s')" %(obj_name, USB)
+        try:
+            logging.warning("[%s], starting collecting info", found_plugin_id);
+            modemobj.get_modem_info()
+            logging.warning("[%s], info collection completed", found_plugin_id);
+        except NameError:
+            logging.error("NameError..");
+
     except (SystemExit, AttributeError):
         logging.warning("Exiting Program due to interrupt")
         sys.exit(1)

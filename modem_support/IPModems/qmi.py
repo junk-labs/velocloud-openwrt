5af99703be402519fa2573f42da0cc66318681d7
Aleksander Morgado <aleksander@aleksander.es>
2015-10-09 20:41:02 +0200

    re#13: IPModems,qmi: allocate CIDs once for each service and reuse them from then on

--- modem_support/IPModems/qmi.py
+++ modem_support/IPModems/qmi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import logging
 import commands
 import IPModems
 
@@ -10,6 +11,9 @@ class Qmi(IPModems.IPModems):
 		self.modem_str = 'qmi'
 		self.timer = 3
 		self.clicmd = '/usr/bin/qmicli -d ' + self.device + ' '
+		self.dms_cid = 0
+		self.nas_cid = 0
+		self.wds_cid = 0
 
 	def runcmd(self, cmd):
 		return commands.getstatusoutput(cmd)[1].strip()
@@ -17,18 +21,75 @@ class Qmi(IPModems.IPModems):
 	def qmicli(self, cmd):
 		return self.runcmd(self.clicmd + cmd)
 
+	def qmicli_dms(self, cmd):
+		if self.dms_cid > 0:
+			return self.runcmd(self.clicmd + "--client-cid=" + str(self.dms_cid) + " --client-no-release-cid " + cmd)
+		else:
+			raise RuntimeError('No DMS client allocated')
+
+	def qmicli_nas(self, cmd):
+		if self.nas_cid > 0:
+			return self.runcmd(self.clicmd + "--client-cid=" + str(self.nas_cid) + " --client-no-release-cid " + cmd)
+		else:
+			raise RuntimeError('No NAS client allocated')
+
+	def qmicli_wds(self, cmd):
+		if self.wds_cid > 0:
+			return self.runcmd(self.clicmd + "--client-cid=" + str(self.wds_cid) + " --client-no-release-cid " + cmd)
+		else:
+			raise RuntimeError('No WDS client allocated')
+
+	def setup(self):
+		IPModems.IPModems.setup(self)
+		try:
+			logging.debug("[dev=%s]: allocating DMS QMI client...", self.USB);
+			self.dms_cid = int(self.qmicli("--dms-noop --client-no-release-cid | sed  -n \"s/.*CID.*'\(.*\)'.*/\\1/p\""))
+		except:
+			logging.warning("[dev=%s]: couldn't allocate DMS QMI client", self.USB);
+		try:
+			logging.debug("[dev=%s]: allocating NAS QMI client...", self.USB);
+			self.nas_cid = int(self.qmicli("--nas-noop --client-no-release-cid | sed  -n \"s/.*CID.*'\(.*\)'.*/\\1/p\""))
+		except:
+			logging.warning("[dev=%s]: couldn't allocate NAS QMI client", self.USB);
+		try:
+			logging.debug("[dev=%s]: allocating WDS QMI client...", self.USB);
+			self.wds_cid = int(self.qmicli("--wds-noop --client-no-release-cid | sed  -n \"s/.*CID.*'\(.*\)'.*/\\1/p\""))
+		except:
+			logging.warning("[dev=%s]: couldn't allocate WDS QMI client", self.USB);
+
+	def teardown(self):
+		IPModems.IPModems.teardown(self)
+		if (self.dms_cid > 0):
+			logging.debug("[dev=%s]: releasing DMS QMI client %d", self.USB, self.dms_cid);
+			self.qmicli("--dms-noop --client-cid=" + str(self.dms_cid))
+		if (self.nas_cid > 0):
+			logging.debug("[dev=%s]: releasing NAS QMI client %d", self.USB, self.nas_cid);
+			self.qmicli("--nas-noop --client-cid=" + str(self.nas_cid))
+		if (self.wds_cid > 0):
+			logging.debug("[dev=%s]: releasing WDS QMI client %d", self.USB, self.wds_cid);
+			self.qmicli("--wds-noop --client-cid=" + str(self.wds_cid))
+
+
 	def get_static_values(self):
-		linkid = self.qmicli("--dms-uim-get-imsi | awk -F\"'\" '{for(i=1;i<=NF;i++){ if(match($i, /[0-9]{14,15}/)){printf $i} } }'")
-		conn = self.qmicli("--wds-get-packet-service-status | awk -F\"'\" '/Connection status/ { print $2}'")
-		modem_name = self.qmicli("--dms-get-manufacturer | awk '/Manufacturer:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
+		# Reset to defaults before reloading
+		self.linkid = 'unknown'
+		self.modem_name = 'unknown'
+		self.modem_version = 'unknown'
 
-		modem_version = self.qmicli("--dms-get-model | awk '/Model:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
+		try:
+			self.linkid = self.qmicli_dms("--dms-uim-get-imsi | awk -F\"'\" '{for(i=1;i<=NF;i++){ if(match($i, /[0-9]{14,15}/)){printf $i} } }'")
+			self.modem_name = self.qmicli_dms("--dms-get-manufacturer | awk '/Manufacturer:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
+			self.modem_version = self.qmicli_dms("--dms-get-model | awk '/Model:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
+			self.connection_status = self.qmicli_wds("--wds-get-packet-service-status | awk -F\"'\" '/Connection status/ { print $2}'")
+		except RuntimeError:
+			logging.warning("[dev=%s]: couldn't load static values", self.USB)
 
-		# Set the values
-		self.linkid = linkid
-		self.connection_status = conn
-		self.modem_name = modem_name
-		self.modem_version = modem_version
+		logging.debug("[dev=%s]: manufacturer: %s", self.USB, self.modem_name);
+		logging.debug("[dev=%s]: model: %s", self.USB, self.modem_version);
+		logging.debug("[dev=%s]: linkid: %s", self.USB, self.linkid);
+		logging.debug("[dev=%s]: connection status (initial): %s", self.USB, self.connection_status);
+
+		# Set the static values
 		self.activation_status = 'activated'
 		self.isp_name = ''
 		self.supported_technologies = 'LTE'
@@ -38,12 +99,10 @@ class Qmi(IPModems.IPModems):
 		#self.dns_value = wandns
 
 	def get_dynamic_values(self):
-		rsrp = self.qmicli("--nas-get-signal-info | awk '/LTE:/ { for(i=1; i<5; i++) { getline; gsub(/\\x27/, \"\", $0); if(match($0, /RSRP:/)) {printf $2 } }}'")
 		try:
-			rsrp = int(rsrp)
+			rsrp = int(self.qmicli_nas("--nas-get-signal-info | awk '/LTE:/ { for(i=1; i<5; i++) { getline; gsub(/\\x27/, \"\", $0); if(match($0, /RSRP:/)) {printf $2 } }}'"))
 		except:
-			rsrp = "-120"
-			pass
+			rsrp = -120
 
 		rxvalue = self.runcmd("ifconfig " + self.ifname + " | awk  '/RX bytes:/ { split($2, a, /:/); print a[2]}'")
 		txvalue = self.runcmd("ifconfig " + self.ifname + " | awk  '/TX bytes:/ { split($6, a, /:/); print a[2]}'")

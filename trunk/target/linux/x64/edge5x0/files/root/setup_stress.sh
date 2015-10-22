22b76f7c9763080902c48543bb0ed891f2133d99
Shankar Unni <shankar@velocloud.net>
2015-10-22 13:50:18 -0700

    Add "lan" network back into stress_loop network setup.
    In case we want to connect to wireless while the stress loop is running.

--- trunk/target/linux/x64/edge5x0/files/root/setup_stress.sh
+++ trunk/target/linux/x64/edge5x0/files/root/setup_stress.sh
@@ -7,6 +7,12 @@ config interface 'loopback'
        option ipaddr '127.0.0.1'
        option netmask '255.0.0.0'
 
+config interface 'lan'
+        option ifname ''
+        option proto 'static'
+        option type 'bridge'
+        option ipaddr '192.168.2.1'
+        option netmask '255.255.255.0'
 EOF
 
 for IFNAME in sw0p0 sw0p1 sw0p2 sw0p3 sw1p0 sw1p1 sw1p2 sw1p3 eth2 eth3 eth4 eth5 ; do

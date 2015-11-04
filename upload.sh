34923bd08f6894055eb3da859dafee49d4e559b6
Aleksander Morgado <aleksander@aleksander.es>
2015-11-04 17:46:54 +0100

    build: helper script to upload updated files to a remote edge device
    This is just for testing purposes.

---
+++ upload.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -z $VELOCLOUD ]; then
+    echo "error: need VELOCLOUD envvar set to the IP address or server to access to"
+    exit 1
+fi
+
+#SSH_PREFS="-o PreferredAuthentications=password"
+SSH_PREFS=""
+
+scp ${SSH_PREFS}                                 \
+    modem_support/30-usb_ip                      \
+    root@${VELOCLOUD}:/etc/hotplug.d/net/
+
+scp ${SSH_PREFS}                                 \
+    modem_support/modem_service.sh               \
+    modem_support/modem_functions.sh             \
+    modem_support/modem.path                     \
+    modem_support/usb_common                     \
+    root@${VELOCLOUD}:/opt/vc/modems/
+
+scp  ${SSH_PREFS}                                \
+    modem_support/IPModems/IPModemsRun.py        \
+    modem_support/IPModems/IPModems.py           \
+    modem_support/IPModems/qmi.py                \
+    modem_support/IPModems/sierra.py             \
+    modem_support/IPModems/uml290.py             \
+    modem_support/IPModems/huawei.py             \
+    root@${VELOCLOUD}:/opt/vc/modems/IPModems/
+
+scp ${SSH_PREFS}                                 \
+    modem_support/IPModems/huaweistatus.gcom     \
+    root@${VELOCLOUD}:/etc/gcom/

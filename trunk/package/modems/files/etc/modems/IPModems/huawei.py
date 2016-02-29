81e6ee9e25de975103216deeb541d6a3c5971e5a
Aleksander Morgado <aleksander@aleksander.es>
2016-02-29 17:10:13 +0100

    huawei: tabify

--- trunk/package/modems/files/etc/modems/IPModems/huawei.py
+++ trunk/package/modems/files/etc/modems/IPModems/huawei.py
@@ -10,11 +10,11 @@ class Huawei(IPModems.IPModems):
 		IPModems.IPModems.__init__(self, USB)
 		self.modem_str = 'huawei'
 		self.timer = 3
-                self.connection_status_check_errors = 0
-                self.connection_status = 'disconnected'
+		self.connection_status_check_errors = 0
+		self.connection_status = 'disconnected'
 
-                # If we have 10 consecutive checks (~30s) without a proper connection, we request assume disconnected
-                self.connection_status_check_threshold = 10
+		# If we have 10 consecutive checks (~30s) without a proper connection, we request assume disconnected
+		self.connection_status_check_threshold = 10
 
 	def runcmd(self, cmd):
 		return commands.getstatusoutput(cmd)[1].strip()
@@ -23,23 +23,23 @@ class Huawei(IPModems.IPModems):
 		try:
 			cmd = "gcom -d " + self.device + " -s /etc/gcom/huaweistatus.gcom"
 			new_connection_status = self.runcmd(cmd)
-                        if new_connection_status == 'connected':
-                                self.connection_status_check_errors = 0
-                                self.connection_status = new_connection_status
-                                return
-                        # If we were already disconnected, nothing to do
-                        if self.connection_status == 'disconnected':
-                                return
-                        # If we're reported disconnected N consecutive times times, flag disconnected
-                        self.connection_status_check_errors += 1
-                        if self.connection_status_check_errors == self.connection_status_check_threshold:
-                                logging.debug("[dev=%s]: too many checks reported disconnection...", self.USB)
-                                self.connection_status_check_errors = 0
-                                self.connection_status = 'disconnected'
-                                return
-                        # Otherwise, do nothing else, we don't consider the new state change to
-                        # disconnected until the 10th try
-                        logging.debug("[dev=%s]: disconnection reported (%d)...", self.USB, self.connection_status_check_errors)
+			if new_connection_status == 'connected':
+				self.connection_status_check_errors = 0
+				self.connection_status = new_connection_status
+				return
+			# If we were already disconnected, nothing to do
+			if self.connection_status == 'disconnected':
+				return
+			# If we're reported disconnected N consecutive times times, flag disconnected
+			self.connection_status_check_errors += 1
+			if self.connection_status_check_errors == self.connection_status_check_threshold:
+				logging.debug("[dev=%s]: too many checks reported disconnection...", self.USB)
+				self.connection_status_check_errors = 0
+				self.connection_status = 'disconnected'
+				return
+			# Otherwise, do nothing else, we don't consider the new state change to
+			# disconnected until the 10th try
+			logging.debug("[dev=%s]: disconnection reported (%d)...", self.USB, self.connection_status_check_errors)
 
 		except RuntimeError:
 			logging.warning("[dev=%s]: couldn't load connection status", self.USB)
@@ -55,10 +55,10 @@ class Huawei(IPModems.IPModems):
 		logging.debug("[dev=%s]: static values...", self.USB)
 
 		cmd = "gcom -d " + self.device + " -s /etc/gcom/getcardinfo.gcom"
-		self.modem_name	   = self.runcmd(cmd + " | awk '/Model:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
+		self.modem_name    = self.runcmd(cmd + " | awk '/Model:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
 		self.modem_version = self.runcmd(cmd + " | awk '/Revision:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
-		self.linkid	   = self.runcmd(cmd + " | awk '/IMEI:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
-		self.isp_name	   = ''
+		self.linkid        = self.runcmd(cmd + " | awk '/IMEI:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
+		self.isp_name      = ''
 		self.activation_status = 'activated'
 
                 # Disable unsolicited messages that flood the ports
@@ -69,6 +69,7 @@ class Huawei(IPModems.IPModems):
 		self.reload_connection_status()
 		logging.debug("[dev=%s]: connection status (initial): %s", self.USB, self.connection_status);
 		if self.connection_status == 'connected':
+			logging.debug("[dev=%s]: explicit disconnection...", self.USB)
 			cmd = "MODE=\"AT^NDISDUP=1,0\" gcom -d " + self.device + " -s /etc/gcom/setmode.gcom"
 			self.runcmd(cmd)
 
@@ -103,8 +104,8 @@ class Huawei(IPModems.IPModems):
 			self.signal_percentage = 100
 
 		# Statistics
-		self.rx_session_bytes = self.runcmd("ifconfig " + self.ifname + " | awk	 '/RX bytes:/ { split($2, a, /:/); print a[2]}'")
-		self.tx_session_bytes = self.runcmd("ifconfig " + self.ifname + " | awk	 '/TX bytes:/ { split($6, a, /:/); print a[2]}'")
+		self.rx_session_bytes = self.runcmd("ifconfig " + self.ifname + " | awk '/RX bytes:/ { split($2, a, /:/); print a[2]}'")
+		self.tx_session_bytes = self.runcmd("ifconfig " + self.ifname + " | awk '/TX bytes:/ { split($6, a, /:/); print a[2]}'")
 
 		# Flag to decide whether we need to bring iface down and perform reconnection
 		wwan_down = False

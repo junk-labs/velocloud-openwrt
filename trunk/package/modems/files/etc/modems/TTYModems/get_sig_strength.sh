3000650b3df81f8310379218dbbec3c6e42081f6
Aleksander Morgado <aleksander@aleksander.es>
2016-05-27 19:41:16 +0200

    modems,tty: consolidate indentation in get_sig_strength.sh

--- trunk/package/modems/files/etc/modems/TTYModems/get_sig_strength.sh
+++ trunk/package/modems/files/etc/modems/TTYModems/get_sig_strength.sh
@@ -4,29 +4,30 @@
 USB=$1
 
 while [[ 1 ]]; do
-        device=$(uci -c $modem_config_path get modems.$USB.mgmt)
-	if [ ! -n "$device" ]; then
-		continue
-	fi
-        value=$(gcom -s /etc/gcom/getstrength.gcom -d $device | grep '+CSQ:' | awk -F":|," '{print $2}')
+    device=$(uci -c $modem_config_path get modems.$USB.mgmt)
+    if [ ! -n "$device" ]; then
+        continue
+    fi
 
-        # Marginal - Levels of -95dBm or lower
-        # Workable under most conditions - Levels of -85dBm to -95dBm
-        # Good - Levels between -75dBm and -85dBm
-        # Excellent - levels above -75dBm.
+    value=$(gcom -s /etc/gcom/getstrength.gcom -d $device | grep '+CSQ:' | awk -F":|," '{print $2}')
 
-        strength=$(expr 113 - $value - $value)
-        strength=-$strength
-        sigpercentage=0
-        if [ $strength -le -95 ]; then sigpercentage=25;
-        elif [[ $strength -le -95 ]] && [[ $strength -ge -85 ]]; then sigpercentage=50;
-        elif [[ $strength -le -85 ]] && [[ $strength -ge -75 ]]; then sigpercentage=75;
-        elif [ $strength -ge -75 ]; then sigpercentage=100;
-        fi
+    # Marginal - Levels of -95dBm or lower
+    # Workable under most conditions - Levels of -85dBm to -95dBm
+    # Good - Levels between -75dBm and -85dBm
+    # Excellent - levels above -75dBm.
 
-        rxbytes=$(ifconfig $USB | awk  '/RX packets:/ { sub(/packets:/, "");print $2}')
-        txbytes=$(ifconfig $USB | awk  '/TX packets:/ { sub(/packets:/, "");print $2}')
+    strength=$(expr 113 - $value - $value)
+    strength=-$strength
+    sigpercentage=0
+    if [ $strength -le -95 ]; then sigpercentage=25;
+    elif [[ $strength -le -95 ]] && [[ $strength -ge -85 ]]; then sigpercentage=50;
+    elif [[ $strength -le -85 ]] && [[ $strength -ge -75 ]]; then sigpercentage=75;
+    elif [ $strength -ge -75 ]; then sigpercentage=100;
+    fi
 
-        echo -n '{ "SigStrength" : '"$strength"', "SigPercentage" : '"$sigpercentage"', "Rxbytes" : '"$rxbytes"', "Txbytes" : '"$txbytes"' }' > "$tmpdir/""$USB"_periodic.txt
-        sleep 3
+    rxbytes=$(ifconfig $USB | awk  '/RX packets:/ { sub(/packets:/, "");print $2}')
+    txbytes=$(ifconfig $USB | awk  '/TX packets:/ { sub(/packets:/, "");print $2}')
+
+    echo -n '{ "SigStrength" : '"$strength"', "SigPercentage" : '"$sigpercentage"', "Rxbytes" : '"$rxbytes"', "Txbytes" : '"$txbytes"' }' > "$tmpdir/""$USB"_periodic.txt
+    sleep 3
 done

6cf22d683f345076bae492c270caae923babd7c4
Aleksander Morgado <aleksander@aleksander.es>
2017-04-27 15:17:27 +0200

    modems,qmihybrid bug 95: always assume the modem is autoconnected
    This plugin only applies to the 1410:9022 modem, so assume it is
    always autoconnected.
    
    (cherry picked from commit 4e93d7d7df37fd16a20f5a9b0b59c6595385d529)

--- trunk/package/modems/files/etc/modems/IPModems/qmihybrid.py
+++ trunk/package/modems/files/etc/modems/IPModems/qmihybrid.py
@@ -12,68 +12,26 @@ class Qmihybrid(IPModems.IPModems):
 		IPModems.IPModems.__init__(self, USB)
 		self.modem_str = 'qmihybrid'
 		self.timer = 3
-		self.connection_status = 'disconnected'
 		self.connection_errors = 0
-		self.ipaddr = 0
+		self.soft_reset_threshold = 20
+		self.hard_reset_threshold = 40
 
-		# If we have 10 consecutive checks (~30s) without a proper connection, we request a hard reset
-		self.hard_reset_threshold = 10
-
-	def runcmd(self, cmd):
-		return commands.getstatusoutput(cmd)[1].strip()
-
-	def reload_connection_status(self):
-		try:
-			# Support auto-connected devices
-			if self.product == '1410:9022':
-				self.hard_reset_threshold = 40
-				logging.debug("[dev=%s]: QMI hybrid modem is set to autoconnect...", self.USB)
-				self.connection_status = 'connected'
-				f = os.popen('ifconfig %s | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1' % self.ifname)
-				self.ipaddr = f.read().rstrip()
-				return
-
-			# Query the modem
-			self.connection_status = 'disconnected'
-			cmd = "gcom -d " + self.device + " -s /etc/gcom/qmihybridstatus.gcom | grep ' CONNECTED'"
-			cmdout = self.runcmd(cmd).strip()
-			if cmdout:
-				self.connection_status = 'connected'
-			else:
-				cmd = "gcom -d " + self.device + " -s /etc/gcom/qmihybridstatus.gcom | grep 'QMI_WDS_PKT_DATA_CONNECTED'"
-				cmdout = self.runcmd(cmd).strip()
-				if cmdout:
-					self.connection_status = 'connected'
-		except RuntimeError:
-			logging.warning("[dev=%s]: couldn't load connection status", self.USB)
-
-	def reconnected(self):
-		# Clear connection errors once connected
-		self.connection_errors = 0
-
-		# Make sure WWAN is always up when just connected
-		logging.debug("[dev=%s]: setting %s device up...", self.USB, self.ifname)
-		self.runcmd("/usr/sbin/ip link set dev " + self.ifname + " up")
-		logging.debug("[dev=%s]: reloading network...", self.USB)
-		self.runcmd("ubus call network reload")
 
 	def get_static_values(self):
 		logging.debug("[dev=%s]: static values...", self.USB)
 
 		cmd = "gcom -d " + self.device + " -s /etc/gcom/getcardinfo.gcom"
-		self.modem_name	   = self.runcmd(cmd + " | awk '/Model:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
+		self.modem_name = self.runcmd(cmd + " | awk '/Model:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
 		self.modem_version = self.runcmd(cmd + " | awk '/Revision:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
-		self.linkid	   = self.runcmd(cmd + " | awk '/IMEI:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
+		self.linkid	= self.runcmd(cmd + " | awk '/IMEI:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
 		if not self.linkid:
 			self.linkid = self.runcmd(cmd + " | awk '/ESN:/ { gsub(/\\x27/, \"\", $0); for(i=2;i<=NF;i++) {printf $i\" \"}}'")
-		self.isp_name	   = ''
+		self.isp_name = ''
 		self.activation_status = 'activated'
 
-		# Support autoconnected devices (i.e. already connected when started)
-		self.reload_connection_status()
-		logging.debug("[dev=%s]: connection status (initial): %s", self.USB, self.connection_status);
-		if self.connection_status == 'connected':
-			self.reconnected()
+		# Autoconnected
+		self.setup_network_interface()
+
 
 	def get_dynamic_values(self):
 		logging.debug("[dev=%s]: dynamic values...", self.USB)
@@ -86,15 +44,15 @@ class Qmihybrid(IPModems.IPModems):
 			if value == 99:
 				self.signal_strength = -120
 			else:
-				self.signal_strength = (-1) * (113 - value - value);
+				self.signal_strength = (-1) * (113 - value - value)
 		except:
 			self.signal_strength = -120
 
 		# Signal percentage
-		#   Marginal - Levels of -95dBm or lower
-		#   Workable under most conditions - Levels of -85dBm to -95dBm
-		#   Good - Levels between -75dBm and -85dBm
-		#   Excellent - levels above -75dBm.
+		#	Marginal - Levels of -95dBm or lower
+		#	Workable under most conditions - Levels of -85dBm to -95dBm
+		#	Good - Levels between -75dBm and -85dBm
+		#	Excellent - levels above -75dBm.
 		self.signal_percentage = 0
 		if self.signal_strength < -95:
 			self.signal_percentage = 25
@@ -109,73 +67,30 @@ class Qmihybrid(IPModems.IPModems):
 		self.rx_session_bytes = self.runcmd("ifconfig " + self.ifname + " | awk	 '/RX bytes:/ { split($2, a, /:/); print a[2]}'")
 		self.tx_session_bytes = self.runcmd("ifconfig " + self.ifname + " | awk	 '/TX bytes:/ { split($6, a, /:/); print a[2]}'")
 
-		# Flag to decide whether we need to bring iface down and perform reconnection
-		wwan_down = False
+		# Check IP address
+		logging.debug("[dev=%s]: checking IP settings in device...", self.USB)
+		f = os.popen('ifconfig %s | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1' % self.ifname)
+		ipaddress = f.read().rstrip()
 
-		# Connection status
-		self.reload_connection_status()
-		if self.connection_status != 'connected':
-			logging.warning("[dev=%s]: not connected in network", self.USB)
-			wwan_down = True
-
-		# If modem has had no ip address for a while, hard reset it 
-		if self.product == '1410:9022':
-			if (self.ipaddr == "") or (self.ipaddr == "192.168.1.2"):
-				self.connection_errors += 1
-                		if self.connection_errors == self.hard_reset_threshold:
-                        		self.runcmd("/usr/sbin/ip link set dev " + self.ifname + " down")
-					time.sleep(5)
-                        		self.runcmd("/usr/sbin/ip link set dev " + self.ifname + " down")
-					time.sleep(5)
-                        		logging.warning("[dev=%s]: too many connection errors (%d), ipaddr (%s): requesting hard reset", self.USB, self.connection_errors, self.ipaddr)
-                        		cmd = "gcom -d " + self.device + " -s /etc/gcom/atreset.gcom"
-                        		self.runcmd(cmd)
-                        		self.connection_errors = 0
-			else:
-				self.connection_errors = 0
-
-		# Do we need to force a full explicit disconnection of all our state info?
-		if wwan_down:
-			# Flag a new connection error detected
+		# If modem has had no ip address for a while, reset it
+		if (ipaddress == "") or (ipaddress == "192.168.1.2"):
 			self.connection_errors += 1
+			self.set_modem_status("Disconnected")
 
-			# If we have WWAN net info, bring it down
-			logging.debug("[dev=%s]: setting %s device down...", self.USB, self.ifname)
-			self.runcmd("/usr/sbin/ip link set dev " + self.ifname + " down")
+			# Only relaunch network interface settings on soft reset
+			if self.connection_errors == self.soft_reset_threshold:
+				self.teardown_network_interface()
+				time.sleep(5)
+				self.setup_network_interface()
 
-			# Get APN from profile
-			myvars = {}
-			with open(self.get_profile_path()) as myfile:
-				for line in myfile:
-					name, var = line.partition("=")[::2]
-					myvars[name.strip()] = var.strip()
-
-			# Launch connection
-			logging.debug("[dev=%s]: restarting connection...", self.USB)
-
-			cmdparams=",,"
-			if myvars['APN']:
-				cmdparams = ",,,,,," + myvars['APN'] + ",,," + myvars['APN_USER'] + "," + myvars['APN_PASS']
-			cmd = "MODE=\"AT$NWQMICONNECT=" + cmdparams + "\" gcom -d " + self.device + " -s /etc/gcom/setmode.gcom"
-			self.runcmd(cmd)
-
-			self.reload_connection_status()
-			if self.connection_status == 'connected':
-				self.reconnected()
-				logging.warning("[dev=%s]: reconnected", self.USB)
-			else:
-				logging.warning("[dev=%s]: couldn't reconnect", self.USB)
-
-		if self.connection_errors == self.hard_reset_threshold:
-			logging.warning("[dev=%s]: too many connection errors (%d): requesting hard reset", self.USB, self.connection_errors)
-
-			# Modem was working, now it seems to be disconnected, I have seen
-			# at times that when modem goes from connected to disconnected state
-			# after being connected for long, it doesnt help trying an NWQMICONNECT
-			# again, it really needs a knock on the head and then a NWQMICONNECT
-			cmd = "gcom -d " + self.device + " -s /etc/gcom/atreset.gcom"
-			self.runcmd(cmd)
-			# After a reset we get a hotplug remove event, so there is no question
-			# of "going back" or anything here, but just code assume we will go back
-			# to the beginning of the while loop
+			# Fully reboot device in hard reset
+			if self.connection_errors == self.hard_reset_threshold:
+				self.teardown_network_interface()
+				time.sleep(5)
+				logging.warning("[dev=%s]: too many connection errors (%d), ipaddr (%s): requesting hard reset", self.USB, self.connection_errors, ipaddress)
+				cmd = "gcom -d " + self.device + " -s /etc/gcom/atreset.gcom"
+				self.runcmd(cmd)
+				self.connection_errors = 0
+		else:
+			self.set_modem_status_connected()
 			self.connection_errors = 0
